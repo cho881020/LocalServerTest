a3ae2f66a18a676c50f6d0e4f8dffe3d576f8329
cho881020 <cho881020@gmail.com>
2017-11-17 18:10:22 +0900

    학생 나이 / 성별 표시 기능 추가

--- app/src/main/java/com/example/user/localservertest/adapter/StudentAdapter.java
+++ app/src/main/java/com/example/user/localservertest/adapter/StudentAdapter.java
@@ -3,6 +3,7 @@ package com.example.user.localservertest.adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,9 @@ import android.widget.TextView;
 import com.example.user.localservertest.R;
 import com.example.user.localservertest.data.User;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by user on 2017-11-17.
@@ -42,8 +45,20 @@ public class StudentAdapter extends ArrayAdapter<User> {
 
         User data = mList.get(position);
 
+        String birthYearStr = data.getBirthDay().substring(0,4);
+        Log.d("생년", birthYearStr);
+
+        int birthYear = Integer.parseInt(birthYearStr);
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+
+        String genderStr = data.getGender() == 1? "남성" : "여성";
+
+        String infoStr = String.format(Locale.KOREA,
+                "%s (%d세, %s)", data.getName(), (currentYear-birthYear+1), genderStr);
+
+
         TextView nameTxt = row.findViewById(R.id.nameTxt);
-        nameTxt.setText(data.getName());
+        nameTxt.setText(infoStr);
 
         return row;
     }
