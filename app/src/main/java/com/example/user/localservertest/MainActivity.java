bffc6775c5112bbed585270cd861dd766f433c73
cho881020 <cho881020@gmail.com>
2017-11-17 12:22:03 +0900

    데이터 가져오기

--- app/src/main/java/com/example/user/localservertest/MainActivity.java
+++ app/src/main/java/com/example/user/localservertest/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.user.localservertest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.user.localservertest.util.ServerUtil;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends BaseActivity {
@@ -26,10 +28,22 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setValues() {
 
-        ServerUtil.test(mContext, new ServerUtil.JsonResponseHandler() {
+        ServerUtil.getAllLectures(mContext, new ServerUtil.JsonResponseHandler() {
             @Override
             public void onResponse(JSONObject json) {
+                try {
+                    boolean result = json.getBoolean("result");
 
+                    if (result) {
+                        Toast.makeText(mContext, "성공입니다.", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(mContext, "실패입니다.", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
--- app/src/main/java/com/example/user/localservertest/util/ServerUtil.java
+++ app/src/main/java/com/example/user/localservertest/util/ServerUtil.java
@@ -15,11 +15,52 @@ import java.util.Map;
 public class ServerUtil {
     private static final String TAG = ServerUtil.class.getSimpleName();
 
-    private final static String BASE_URL = "http://192.168.20.85:8080/"; // 라이브서버
+    private final static String BASE_URL = "http://192.168.20.67:8080/tje/"; // 라이브서버
 
     public interface JsonResponseHandler {
         void onResponse(JSONObject json);
     }
+
+
+    public static void getAllLectures(final Context context, final JsonResponseHandler handler) {
+        String url = BASE_URL+"get_all_lectures";
+        //		String registrationId = ContextUtil.getRegistrationId(context);
+
+        Map<String, String> data = new HashMap<String, String>();
+        data.put("uid_list", "[11]");
+
+        AsyncHttpRequest.get(context, url,  data, true, new AsyncHttpRequest.HttpResponseHandler() {
+
+            @Override
+            public boolean onPrepare() {
+                return true;
+            }
+
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response);
+                try {
+                    JSONObject json = new JSONObject(response);
+
+                    if (handler != null)
+                        handler.onResponse(json);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFinish() {
+
+            }
+
+            @Override
+            public void onCancelled() {
+
+            }
+
+        });
+    }
+
 
 
     public static void test(final Context context, final JsonResponseHandler handler) {
